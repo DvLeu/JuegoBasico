2c2042fef1e94a93d72beb12b73d8b622ba55b57
David León <108154987+DvLeu@users.noreply.github.com>
2023-05-15 10:03:00 -0600

    Proyecto Creado : El objeto Personaje.java es agregado y configurado con atributos basicos para su implementacion.

---
+++ src/main/java/Hilos/Personaje.java
@@ -0,0 +1,72 @@
+package Hilos;
+import java.util.Random;
+
+public class Personaje {
+    private Random random = new Random(System.nanoTime());
+    private int vida;
+    private int fisico;
+    private int magia;
+    private int dinero;
+    private int nivel;
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getFisico() {
+        return fisico;
+    }
+
+    public void setFisico(int fisico) {
+        this.fisico = fisico;
+    }
+
+    public int getMagia() {
+        return magia;
+    }
+
+    public void setMagia(int fuerza) {
+        this.magia = fuerza;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    //Ataques fisicos
+    public int ataqueFisico(){
+        int randi = random.nextInt(2);
+        return (nivel * fisico * randi);
+    }
+    //Ataques magicos
+    public int ataque_fuerza(){
+        if (magia > 0){
+            magia--;
+            return nivel*magia;
+        }
+        else{
+
+            return 0;
+        }
+    }
+    public void danio(int vida){
+        this.vida -= vida;
+    }
+
+
+}
